99d1d83ca5a22bfffd8a6806dc4223dc8b616486
Ajeet Shah <ajeets.shah@hotmail.com>
2017-01-19 21:30:00 +0530

    Add functions teen, abs, leap

--- src/clojure_exercise/core.clj
+++ src/clojure_exercise/core.clj
@@ -7,11 +7,52 @@
   (println "This line is in the foo")
 )
 
+(defn teen
+  [age]
+  (if (and (> age 12) (< age  20))
+    (println true)
+    (println false)
+  )
+)
+
+(defn abs
+  [number]
+  (if (> number 0)
+    (println number)
+    (println (- number))
+  )
+)
+
+(defn leap
+  [year]
+  (if (= (rem year 400) 0)
+    (println true)
+    (if (= (rem year 100) 0)
+      (println false)
+      (if (= (rem year 4) 0)
+        (println true)
+        (println false)
+      )
+    )
+  )
+)
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!")
   (println "Hello, New World")
   (-foo)
+
+  (teen 17)
+  (teen 22)
+
+  (abs 9)
+  (abs -5)
+
+  (leap 2012)
+  (leap 2000)
+  (leap 4)
+  (leap 1900)
 )
 
